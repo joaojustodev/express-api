46c3a25516c1671846bb49ba2919260fb6fba7c0
joaobr200 <joaoarantesjob@gmail.com>
2020-10-07 08:58:42 -0300

    app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import { router } from "./routes";
@@ -7,11 +7,9 @@ import { router } from "./routes";
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use("/api/v1", router);
-
-
 
 export { app };
