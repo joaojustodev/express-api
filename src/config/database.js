e37e8e9aeea1e51465dea6fede85c6757c28769b
joaobr200 <joaoarantesjob@gmail.com>
2020-10-07 09:08:03 -0300

    eslint-fix:database config

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,9 @@ const prod = process.env.NODE_ENV === "production";
 
 let dbConfig;
 
-if(!prod) {
+if (!prod) {
   dbConfig = {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
       filename: path.resolve(__dirname, "..", "database", "database.sqlite")
     },
@@ -17,28 +17,26 @@ if(!prod) {
       directory: path.resolve(__dirname, "..", "database", "seeds")
     },
     useNullAsDefault: true
-  }
-
+  };
 } else {
   dbConfig = {
-    client: 'postgres',
+    client: "postgres",
     connection: {
       port: process.env.DATABASE_PORT,
       host: process.env.DATABASE_HOST,
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASS,
+      password: process.env.DATABASE_PASS
     },
     migrations: {
       directory: path.resolve(__dirname, "..", "database", "migrations"),
-      tableName: 'knex_migrations',
+      tableName: "knex_migrations"
     },
     seeds: {
       directory: path.resolve(__dirname, "..", "database", "seeds")
     },
     useNullAsDefault: true
-  } 
+  };
 }
 
 export { dbConfig };
-
